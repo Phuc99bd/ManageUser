70a3bd476aa683b6df72cd19ee5a587c379f523f
Cong Phuc <phuccog@gmail.com>
2020-03-27 21:25:29 +0700

    Updates

---
+++ precache-manifest.4d7dd50e2a87adf393ffd4b0d8431e9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e418463c80035ea570b6af39b2cea5e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "017217ba33e0e99b9553",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ef6fcb72a4b478c24e62",
+    "url": "/static/js/2.19ddbdde.chunk.js"
+  },
+  {
+    "revision": "c8a3dabb9ca12628c50b6d4316456afc",
+    "url": "/static/js/2.19ddbdde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "017217ba33e0e99b9553",
+    "url": "/static/js/main.416ad02e.chunk.js"
+  },
+  {
+    "revision": "2be4edb1883050656190",
+    "url": "/static/js/runtime-main.229453ca.js"
+  }
+]);
